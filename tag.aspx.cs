17c6ce217586a8cf79953555f42e93ba0736c35c
Furkan Topcuoğlu <32601126+furkantopcuoglu@users.noreply.github.com>
2020-04-15 23:26:52 +0300

    Create tag.aspx.cs

---
+++ tag.aspx.cs
@@ -0,0 +1,23 @@
+using System.Data;
+using System.Data.SqlClient;
+      sql bgl = new sql();
+      protected void taggonder_Click(object sender, EventArgs e)
+        {
+          // Gelen keliemleri küçük harfe çeviriyor
+            tag.Text = tag.Text.ToLower();
+          // Gelen kelime dizisini her biz dizi elemanını , ile ayrıldığını belirtiyorum
+            string[] tags = tag.Text.Split(',');
+            foreach (var t in tags)
+            {
+               
+                SqlCommand newtag = new SqlCommand("insert into tag(tagname) values(@1)", bgl.baglanti());
+                newtag.Parameters.AddWithValue("@1", t);
+                newtag.ExecuteNonQuery();
+    
+             }
+        }
+        
+                          // Veritabanı //
+        // tag isimli bir tablo
+        // id int primary key auto_increment
+        // tagname varchar(50)
